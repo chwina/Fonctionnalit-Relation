8b8257e81a3106c53b9d39d827d7880df2d131bf
Channa <mchanna@deloitte.fr>
2020-06-30 09:30:33 +0100

    initial commit

---
+++ force-app/main/default/lwc/finalfoyercomponent/finalfoyercomponent.js
@@ -0,0 +1,240 @@
+import { LightningElement,api,track } from 'lwc';
+import cytos from '@salesforce/resourceUrl/cytos_label_min';
+import { loadScript } from 'lightning/platformResourceLoader';
+import getData from '@salesforce/apex/SchemaCtrlMdt.getData'
+export default class Finalfoyercomponent extends LightningElement {
+      
+    @track foyers;
+    @track nombre;
+    @api recordId;
+    @api edges1 = [];
+    @api nodes1 = [];
+     
+    renderedCallback() {
+
+        Promise.all([
+            loadScript(this, cytos + '/cytos2.js'),
+        ])
+            .then(() => {
+                this.initializecytos();
+            })
+           
+    }
+
+    initializecytos(){
+        getData({record : this.recordId})
+        .then(result => {
+           this.foyers =  JSON.parse(result);  
+              this.nombre = (this.foyers).length;     
+        for(var i = 0 ; i< this.nombre ; i++){ 
+         
+          if((this.foyers)[i].groupe =='nodes'){  
+            console.log('acclist'+(this.foyers)[i].dat.acclist);
+              this.nodes1.push({ 
+                data: { 
+                    id: (this.foyers)[i].dat.acc.Id, 
+                    name: (this.foyers)[i].dat.acc.Name,
+                    acc: (this.foyers)[i].dat.acc,
+                    accList : (this.foyers)[i].dat.acclist,
+                    codeCouleur : (this.foyers)[i].dat.codeCouleur,
+                    Backgroundfit : (this.foyers)[i].dat.Backgroundfit,
+                    shape : (this.foyers)[i].dat.shape,
+                    Width : (this.foyers)[i].dat.width+'px',
+                    height : (this.foyers)[i].dat.height+'px',
+                    Fontsize  : (this.foyers)[i].dat.Fontsize+'px',
+                },
+                classes: (this.foyers)[i].dat.acc.Id == this.recordId ? 'init':''
+            });
+          }
+          else{
+            
+             this.edges1.push({
+                data: { id: 'e'+(this.foyers)[i].data2.source+(this.foyers)[i].data2.target,
+                source:(this.foyers)[i].data2.source, 
+                target: (this.foyers)[i].data2.target, 
+                linecolor :(this.foyers)[i].data2.lineColor,
+                couleurEdge : (this.foyers)[i].data2.edgeColor,
+                curvestyle :  (this.foyers)[i].data2.curvestyle,
+                textrotation : (this.foyers)[i].data2.textrotation,
+                Fontsize  :  (this.foyers)[i].data2.Fontsize+'px',
+                label : 'est parent de'
+            },
+            classes:'background'
+             }) ;
+         }
+         
+      } 
+
+      let cy = cytoscape({
+        
+        container: this.template.querySelector('.cy'),  
+        boxSelectionEnabled: true, 
+        ready: function(event){
+          console.log('ready');
+          
+        }, 
+        elements: {
+            nodes : this.nodes1,
+            edges : this.edges1
+        },
+        style: cytoscape.stylesheet()
+        .selector('edge')
+        .css({
+              'width': 1,
+              'line-color': 'data(linecolor)',
+              'target-arrow-color': 'black',
+              'target-arrow-shape': 'triangle',
+              'arrow-scale':1,
+              'curve-style': 'data(curvestyle)', 
+              'text-rotation' : 'data(textrotation)',
+              'text-wrap' :'wrap',
+              'text-max-width' :'100px',
+              'control-point-distance': 40,
+              'label': 'data(label)',
+              'text-valign': 'top',
+              'word-wrap': 'break-word',
+              'font-size': 'data(Fontsize)',
+              'line-height': '1.7px',
+              'color': 'data(couleurEdge)' 
+          
+          }) 
+          .selector('.background') 
+          .css({ 
+              'text-background-opacity' : 1,
+              'text-background-color':'white',
+          }) 
+          .selector('node') 
+          .css({ 
+              'content':'data(name)', 
+              'text-valign': 'center',
+              'color': 'data(codeCouleur)',   
+              'background-fit': 'data(Backgroundfit)', 
+              'shape': 'data(shape)', 
+              'width':'data(Width)',
+              'height':'data(height)',
+              'text-wrap' :'wrap',
+              'text-max-width' :'110px',
+              'font-size': 'data(Fontsize)', 
+              'background-color' : '#B0C4DE'
+          }) 
+          .selector('.init')
+          .css({ 
+             "background-color":"#0169D9",
+              "line-color":"#0169D9",
+              "source-arrow-color":"#0169D9",
+              "target-arrow-color":"#0169D9",
+              "mid-source-arrow-color":"#0169D9",
+              "mid-target-arrow-color":"#0169D9"
+           })
+           .selector('node:selected')
+           .css({
+            "border-width": 2,
+            "border-style": "solid",
+            "border-color": "#3f3f3f",
+            "border-opacity": 1
+           }), 
+        layout: {
+            name: 'circle',
+            rows: 4,
+            animate: 'end',
+						animationDuration: 3000,
+						animationEasing: 'spring(500, 40)'
+        },
+        
+       
+    });
+    
+    var layout = cy.layout({
+        name: 'circle',
+        
+      });           
+      layout.run();
+     
+      cy.nodeHtmlLabel([{
+        query: 'node',
+        cssClass: 'cy-title',
+        valign: "top",
+        valignBox: "top",  
+        
+        tpl(data){  
+           
+            var acc2 = data.acc.Name;
+            var accUrl = data.acc.id;
+         
+           var label1 ='Conseillé: ';
+           var label2 ='Etat: ';
+           var info2 = 'test';
+           var urlImage = 'https://damienflandrin.fr/storage/tutoriel-realiser-une-carte-en-css/card-image.jpg';
+           var iddiv = data.id + 'myUl';
+            return   '<article id="'+data.id+'" style="visibility: hidden; margin-left : -60px; cursor: pointer; position : relative;" class="slds-card slds-m-top_large slds-card_boundary"  >'+
+            '<div class="slds-card__header slds-grid">'+
+                '<header class="slds-media slds-media_center slds-has-flexi-truncate">'+
+                    '<div class="slds-media__figure" style = "width : 60px; height : 65px;">'+
+                        '<img src="'+urlImage+'" />'+
+                    '</div>'+
+                    '<div class="slds-media__body" style = "margin-top : -30px;   width : 60px;">'+
+                        '<h2 class="slds-card__header-title" style = "font-size : 10px;text-align:center; margin-left :-10px;">'+
+                            '<a href="/'+accUrl+'">'+ 
+                                '<span style= "word-break: break-word;">'+acc2+'</span>'+
+                            '</a>'+
+                        '</h2>'+
+                    '</div>'+
+                '</header>'+
+            '</div>'+
+            '<div class="slds-card__body slds-card__body_inner" Style=" border-top-style: outset; text-align:center;padding: 10px 10px;font-weight: bold;box-sizing: border-box; margin-top : -30px;">'+
+               '<div id="'+iddiv+'"></div>'+
+            '</div>'+
+            
+        '</article>';
+            
+
+                                              
+        }
+    }],
+    {enablePointerEvents:true}
+); 
+    
+cy.on('tap','node', function(e){  
+  
+    var ele = e.target;
+    var sl = ele.data('accList');
+    var slides = sl.split(',');
+    console.log('sl'+ele.data('accList'));
+    var iddiv = ele.id() + 'myUl';
+    if ( document.getElementById(iddiv).innerHTML === "" ) {
+    slides.forEach(function(item) {
+     var li = document.createElement("p");
+     var text = document.createTextNode(item);
+     li.appendChild(text);
+     document.getElementById(iddiv).appendChild(li);
+    });
+     
+    
+}
+     
+    
+    var visible = document.getElementById(ele.id()).style.visibility; 
+    document.getElementById(ele.id()).style.visibility = visible == 'hidden'? 'visible' : 'hidden';
+   
+   
+    
+});
+
+window.onscroll = function() {
+    cy.resize();
+};
+
+
+
+        });
+    
+    
+
+     
+
+    }
+  
+
+      
+
+}
---
+++ force-app/main/default/lwc/paraminterface/paraminterface.js
@@ -0,0 +1,286 @@
+import { LightningElement,track, api } from 'lwc';
+import updateParamNodevalues from '@salesforce/apex/ParamClass.updateParamNodevalues';
+import updateParamEdgevalues from '@salesforce/apex/ParamClass.updateParamEdgevalues';
+import updateObjectParamvalues  from '@salesforce/apex/ParamClass.updateObjectParamvalues';
+import getAllObjects from '@salesforce/apex/ParamClass.getAllObjects';
+import getAllchildObjects from '@salesforce/apex/ParamClass.getAllchildObjects';
+import getFields from  '@salesforce/apex/ParamClass.getFields';
+import getFieldsReference from  '@salesforce/apex/ParamClass.getFieldsReference';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+
+export default class Paraminterface extends LightningElement {
+    
+    @track multiple = true;
+    @track backgroundfitvalue = 'none';
+    @track shapevalue = 'rectangle';
+    @track curvevalue = 'bezier';
+    @track Objectvalue ;
+    @track Relationvalue = 'Relation';
+    @track textrotation = 'autorotate';
+
+    @track nodecolor;
+    @track nodefontsize;
+    @track nodewidth;
+    @track nodeheight;
+    @track Edgecolor;
+    @track Edgefontsize;
+    @track Edgelinecolor;
+    @track _selectedfields =[];
+    @track _selectedlookup =[];
+   
+     @api obs = [];
+     @track objects = [];
+     @track relationsoptions = [];
+     @track Relationobjects = [];
+     @track fieldsoptions = [];
+     @track relationfieldsoptions = [];
+     @track parentRelation = [];
+     @track lookupRelation = [];
+
+     @track parentrelationvalue;
+     @track lookuprelationvalue;
+       
+    connectedCallback(){
+        getAllObjects()
+            .then(result => {
+                this.obs = result;
+
+            })
+            .catch(error => {
+                this.error = error;
+            });
+        }
+  
+    handleChangefields(e) {
+        this._selectedfields = e.detail.value;
+        console.log('selectedfields'+this._selectedfields);
+    }
+
+    
+
+    handleChangelookup(e) {
+        this._selectedlookup = e.detail.value;
+        console.log('selectedlookup'+this._selectedlookup);
+        
+    }
+
+    edgelinecolor(event){
+        this.Edgelinecolor = event.target.value;
+        console.log('edgecolorline'+this.Edgelinecolor);
+     }
+    edgefontsizeChange(event){
+        this.Edgefontsize = event.target.value;
+        console.log('edgefontsize'+this.Edgefontsize);
+     }
+    edgecolorChange(event){
+
+        this.Edgecolor = event.target.value;
+        console.log('edgecolor'+this.Edgecolor);
+     }
+    nodecolorChange(event){
+
+        this.nodecolor= event.target.value;
+        console.log('nodecolor'+this.nodecolor);
+     }
+
+     nodefontsizeChange(event){
+
+        this.nodefontsize= event.target.value;
+        console.log('nodefontsize'+this.nodefontsize);
+     }
+     nodewidthChange(event){
+
+        this.nodewidth= event.target.value;
+        console.log('nodewidth'+this.nodewidth);
+     }
+     nodeheightChange(event){
+
+        this.nodeheight= event.target.value;
+        console.log('nodeheight'+this.nodeheight);
+     }
+
+
+    get backgroundfitoptions() {
+        return [
+                 { label: 'cover', value: 'cover' },
+                 { label: 'contain', value: 'contain' },
+                 { label: 'none', value: 'none' },
+               ];
+    }
+    handleChangebackgroundfit(event) {
+            this.backgroundfitvalue = event.detail.value;
+            console.log('backgroundfit'+this.backgroundfitvalue);
+         }
+    
+         get Objectoptions() {
+             console.log('rehqdjqfkj');
+             let options = [];
+               
+                for(let key in (this.obs)) {
+                
+                       options.push({label:key , value:(this.obs)[key]});
+                    
+                }
+               
+            return options;   
+        
+        }
+        handleChangeObject(event) {
+            this.Objectvalue = event.detail.value;
+                console.log('Objectoption'+this.Objectvalue);
+                getFields({objectName : this.Objectvalue })
+                .then(data => {
+                    let fieoptions = [];
+                    for(let key in data){
+                        fieoptions.push({label : data[key],value : data[key]});
+                    }
+                     this.fieldsoptions = fieoptions;
+                })
+                .catch(error => {
+                    this.error = error;
+                }); 
+                
+                getAllchildObjects({parentobject : this.Objectvalue})
+                .then(data => {
+                    this.Relationobjects = data;
+                    console.log('relation2'+this.Relationobjects);
+                    let relationsoptions = [];
+            const listoptions = [];
+            var count = {};
+            Object.values((this.Relationobjects)).forEach(function(i) { count[i] = (count[i]||0) + 1;});
+            console.log(count);
+            for (var element in count) {
+
+                console.log(element + ' = ' + count[element]);
+              } 
+               for(let key in (this.Relationobjects)) {
+                   if(count[(this.Relationobjects)[key]] > 1){
+                       listoptions.push((this.Relationobjects)[key]);
+                    }
+                }
+               let unique = listoptions.filter((item,index) => {return listoptions.indexOf(item) === index});
+               console.log('listoptions'+unique);
+               for(let el in unique){
+                relationsoptions.push({label:unique[el] , value:unique[el]});
+               }
+                  this.relationsoptions = relationsoptions;
+                })
+                .catch(error => {
+                    this.error = error;
+                }); 
+             }
+
+             handleChangeparentrelation(event) {
+                this.parentrelationvalue = event.detail.value;
+                console.log('parent'+this.parentrelationvalue);
+            }
+            handleChangelookuprelation(event) {
+                this.lookuprelationvalue = event.detail.value;
+                console.log('lookup'+this.lookuprelationvalue);
+    }
+            
+      
+        handleChangeRelation(event) {
+                    this.Relationvalue = event.detail.value;
+                    console.log('Relation'+this.Relationvalue);
+                    getFields({objectName : this.Relationvalue })
+                .then(data => {
+                    let fierelationoptions = [];
+                    for(let key in data){
+                        fierelationoptions.push({label : data[key],value : data[key]});
+                    }
+                     this.relationfieldsoptions = fierelationoptions;
+                })
+                .catch(error => {
+                    this.error = error;
+                }); 
+                getFieldsReference({objectName : this.Relationvalue })
+                .then(data => {
+                    let Referencerelation = [];
+                    for(let key in data){
+                        Referencerelation.push({label : data[key],value : data[key]});
+                    }
+                     this.parentRelation = Referencerelation;
+                     this.Referencerelation = Referencerelation;
+                })
+                .catch(error => {
+                    this.error = error;
+                }); 
+                 }
+     get shapeoptions() {
+            return [
+                     { label: 'triangle', value: 'cover' },
+                     { label: 'ellipse', value: 'ellipse' },
+                     { label: 'rectangle', value: 'rectangle' },
+                   ];
+        }
+        handleChangeshape(event) {
+                this.shapevalue = event.detail.value;
+                console.log('shape'+this.shapevalue);
+             }
+        get curveoptions() {
+                return [
+                         { label: 'bezier', value: 'bezier' },
+                         { label: 'unbundled-bezier', value: 'unbundled-bezier' },
+                         { label: 'haystack', value: 'haystack' },
+                       ];
+            }
+        handleChangecurve(event) {
+                    this.curvevalue = event.detail.value;
+                    console.log('curve'+this.curvevalue);
+        }
+        get textrotationoptions() {
+            return [
+                     { label: 'autorotate', value: 'autorotate' },
+                   ];
+        }
+    handleChangetextrotate(event) {
+                this.textrotation = event.detail.value;
+                console.log('textrotate'+this.textrotation);
+    }
+
+    handleClick(e) {
+        Promise.all([
+            updateParamNodevalues({
+                color: this.nodecolor,
+                Backgroudfit: this.backgroundfitvalue,
+                fontsize : this.nodefontsize,
+                shape : this.shapevalue ,
+                width : this.nodewidth,
+                 height : this.nodeheight
+            }),
+            updateParamEdgevalues({
+                color: this.Edgecolor,
+                linecolor: this.Edgelinecolor,
+                fontsize : this.Edgefontsize,
+                curve : this.curvevalue ,
+                textrotation : this.textrotation
+            }),
+            updateObjectParamvalues({
+                Objectname : this.Objectvalue,
+                RelationName : this.Relationvalue,
+                nodesfields :   this._selectedfields,
+                edgefields : this._selectedlookup,
+                ParentRelation : this.parentrelationvalue,
+                Lookuprelation : this.lookuprelationvalue
+            }),
+            
+         ])
+        .then(() => {
+            const evt = new ShowToastEvent({
+                title: 'Record Update',
+                message: 'Custom setting is loaded ',
+                variant: 'success',
+                mode: 'dismissable'
+            });
+            this.dispatchEvent(evt);
+            location.reload();
+        })
+        .catch((error) => {
+            this.message = 'Error received: code'+error;
+        });
+    }
+
+
+
+}
